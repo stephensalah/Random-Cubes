115a44d11fc7bffbface6d3da8e838d8b0a1f4fb
stephensalah <stephen@salahfamily.org>
2022-01-24 15:10:07 -0600

    Cube Setup
    -Created Cube Prefab
    -Still need to finish the C# code for script

---
+++ Random-Cubes-Unity/Assets/Scripts/RandomCube.cs
@@ -0,0 +1,27 @@
+/* Created by Steve Salah
+ * Date Created: January 24, 2022
+ * 
+ * Last Edited January 24, 2022
+ * Last Edited by Steve Salah
+ * 
+ * Description: Spawn multiple cubes into the scene.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
